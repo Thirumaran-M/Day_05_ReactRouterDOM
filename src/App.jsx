8bdd822ea8c2de852e33e55055fb532a6c12fcb0
Thirumaran-M <maranletters@gmail.com>
2024-04-01 10:08:50 +0530

    Image UPdated

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ import Career from './Career'
 
 function App() {
 	let Input = [
-		{ id: 1, title: "Best Full-Stack Development Project Ideas in 2024", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/person-front-computer-working-html-1200x801.jpg", name: "FSD" },
+		{ id: 1, title: "Best Full-Stack Development Project Ideas in 2024", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp", name: "FSD" },
 		{ id: 2, title: "How Long Would It Take to Be a Full Stack Developer?", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp", name: "FSD" },
-		{ id: 3, title: "How does Apache work? A detailed introduction to Apache", image: "https://www.guvi.in/blog/wp-content/uploads/2023/02/1_6rDcIgFJQldloIERiUSmzw-768x317.png", name: "FSD" },
-		{ id: 4, title: "6 Essential Prerequisites For Learning ReactJS", image: "https://www.guvi.in/blog/wp-content/uploads/2021/05/react-600x314.jpg", name: "FSD" },
+		{ id: 3, title: "How does Apache work? A detailed introduction to Apache", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp", name: "FSD" },
+		{ id: 4, title: "6 Essential Prerequisites For Learning ReactJS", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp", name: "FSD" },
 		{ id: 5, title: "12 Real-World Data Science Examples: Power Of Data Science", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/Real-World-Data-Science-Examples-768x402.webp", name: "DS" },
 		{ id: 6, title: "Top 10 High Paying Non-Coding Jobs in Data Science in 2024", image: "https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science-768x480.webp", name: "DS" },
 		{ id: 7, title: "Can A Commerce Student Do Data Science?", image: "https://www.guvi.in/blog/wp-content/uploads/2022/06/5.-data-science-768x402.png", name: "DS" },
