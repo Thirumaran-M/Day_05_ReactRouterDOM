80604a6de7945b39cd6e7805cb36841de76e031c
Thirumaran-M <maranletters@gmail.com>
2024-04-01 10:13:29 +0530

    Images Updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,16 @@ function App() {
 		{ id: 4, title: "6 Essential Prerequisites For Learning ReactJS", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp", name: "FSD" },
 		{ id: 5, title: "12 Real-World Data Science Examples: Power Of Data Science", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/Real-World-Data-Science-Examples-768x402.webp", name: "DS" },
 		{ id: 6, title: "Top 10 High Paying Non-Coding Jobs in Data Science in 2024", image: "https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science-768x480.webp", name: "DS" },
-		{ id: 7, title: "Can A Commerce Student Do Data Science?", image: "https://www.guvi.in/blog/wp-content/uploads/2022/06/5.-data-science-768x402.png", name: "DS" },
-		{ id: 8, title: "Roles and Responsibilities of a Data Scientist", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/image-4-768x433.png", name: "DS" },
+		{ id: 7, title: "Can A Commerce Student Do Data Science?", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/Real-World-Data-Science-Examples-768x402.webp", name: "DS" },
+		{ id: 8, title: "Roles and Responsibilities of a Data Scientist", image: "https://www.guvi.in/blog/wp-content/uploads/2023/07/Real-World-Data-Science-Examples-768x402.webp", name: "DS" },
 		{ id: 9, title: "Cybersecurity vs Artificial Intelligence | Better Career", image: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*0-yTmT550d7IlPqvFtFJOg.png", name: "CS" },
-		{ id: 10, title: "Cybersecurity Vs Ethical Hacking: Top 10 Differences", image: "https://www.guvi.in/blog/wp-content/uploads/2022/12/Yellow-Blue-Illustration-Business-Blog-Banner-768x432.png", name: "CS" },
+		{ id: 10, title: "Cybersecurity Vs Ethical Hacking: Top 10 Differences", image: "https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-768x480.webp", name: "CS" },
 		{ id: 11, title: "What Is Hacking? Types of Hacking & More", image: "https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-768x480.webp", name: "CS" },
-		{ id: 12, title: "Top 7 Cyber Security Attacks in Real Life", image: "https://www.guvi.in/blog/wp-content/uploads/2022/06/Types-of-Cyber-Security-Attacks-and-How-to-Minimize-the-Impact-768x240.jpg", name: "CS" },
-		{ id: 13, title: "How to Render an Array of Objects in React?", image: "https://www.guvi.in/blog/wp-content/uploads/2022/12/Render-array-of-objects-in-ReactJs-768x432.png", name: "CAR" },
-		{ id: 14, title: "6 Reasons Business Analytics Makes Digital Marketing Powerful", image: "https://www.guvi.in/blog/wp-content/uploads/2022/10/31958-1536x658.jpg", name: "CAR" },
+		{ id: 12, title: "Top 7 Cyber Security Attacks in Real Life", image: "https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-768x480.webp", name: "CS" },
+		{ id: 13, title: "How to Render an Array of Objects in React?", image: "https://www.guvi.in/blog/wp-content/uploads/2024/02/Feature-Image-Top-Digital-Marketing-Trends-1-768x286.webp", name: "CAR" },
+		{ id: 14, title: "6 Reasons Business Analytics Makes Digital Marketing Powerful", image: "https://www.guvi.in/blog/wp-content/uploads/2024/02/Feature-Image-Top-Digital-Marketing-Trends-1-768x286.webp", name: "CAR" },
 		{ id: 15, title: "Top Digital Marketing Trends for 2024", image: "https://www.guvi.in/blog/wp-content/uploads/2024/02/Feature-Image-Top-Digital-Marketing-Trends-1-768x286.webp", name: "CAR" },
-		{ id: 16, title: "Best Websites to Learn Digital Marketing in 2024", image: "https://www.guvi.in/blog/wp-content/uploads/2022/10/Best-Websites-to-Learn-DIgital-Marketing-in-2023-768x432.jpg", name: "CAR" }
+		{ id: 16, title: "Best Websites to Learn Digital Marketing in 2024", image: "https://www.guvi.in/blog/wp-content/uploads/2024/02/Feature-Image-Top-Digital-Marketing-Trends-1-768x286.webp", name: "CAR" }
 	];
 
 	return (
